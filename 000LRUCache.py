f2c180545535b6af54c0832b4e523240f6053727
Justin Tian <tkg8w58@kohls.com>
2018-10-16 14:47:50 -0700

    146. LRU Cache

---
+++ 000LRUCache.py
@@ -0,0 +1,93 @@
+# 146. LRU Cache
+
+# Google Tag
+
+'''
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+'''
+
+class LRUCache:
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+
+        from collections import deque
+        self.d = {}
+        self.l = capacity
+        self.q = deque()
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        
+        if key in self.d:
+            self.q.remove(key)
+            self.q.append(key)
+            return self.d[key]
+        else:
+            return -1
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+
+        self.d[key] = value
+            
+        if key in self.q:
+            self.q.remove(key)
+        elif len(self.q) == self.l:
+            t = self.q.popleft()
+            self.d.pop(t)
+        self.q.append(key)
+
+        return
+
+    ################################################################################################
+    # O(1)
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
