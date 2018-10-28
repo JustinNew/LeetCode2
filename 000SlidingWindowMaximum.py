b0b31d92b5465368e3c9943b66e5ffa8c6b9cb0f
Justin Tian <tkg8w58@kohls.com>
2018-10-28 14:45:23 -0700

    239. Sliding Window Maximum

--- 000SlidingWindowMaximum.py
+++ 000SlidingWindowMaximum.py
@@ -1,5 +1,32 @@
 # 239. Sliding Window Maximum
 
+# Google Tag
+
+'''
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+
+Example:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7] 
+Explanation: 
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
+
+Follow up:
+Could you solve it in linear time?
+'''
+
 class Solution:
     def maxSlidingWindow(self, nums, k):
         """
@@ -7,71 +34,55 @@ class Solution:
         :type k: int
         :rtype: List[int]
         """
-
-        import heapq
+        import bisect
 
         if len(nums) == 0:
             return []
 
-        window = [-1 * i for i in nums[:k]]
         res = []
+        s = nums[:k]
+        s.sort()
 
-        heapq.heapify(window)
         for i in range(k, len(nums)):
-            res.append(-1 * window[0])
-            window.remove(-1 * nums[i - k])
-            heapq.heapify(window)
-            heapq.heappush(window, -1 * nums[i])
+            res.append(s[-1])
+            s.remove(nums[i - k])
+            bisect.insort(s, nums[i])
 
-        res.append(-1 * window[0])
+        res.append(s[-1])
 
         return res
 
     '''
-    Try to get it in linear time.
-    Use stack to keep the non-increase list of maximum number.
+    1. Use deque
+    2. Keep the index of decreasing max
     '''
-
     def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
 
         from collections import deque
-        
-        if len(nums) == 0:
+
+        s = deque()
+        l = len(nums)
+        if l == 0:
             return []
-        
-        m = deque()
+
         res = []
-
-        for i in range(k):
-            if len(m) == 0:
-                m.append(nums[i])
-            elif nums[i] <= m[-1]:
-                m.append(nums[i])
-            else:
-                while len(m) != 0 and m[-1] < nums[i]:
-                    m.pop()
-                m.append(nums[i])
-
-        res.append(m[0])
+        for i in range(l): 
+            # Pop out from left out of range index.
+            while len(s) > 0 and s[0] <= i - k:
+                s.popleft()
             
-        for i in range(k, len(nums)):
-
-            # Need to pop out nums[i - k]
-            # If m[0] equal it, pop out first.
-            if m[0] == nums[i - k]:
-                m.popleft()
-
-            # Update maximum list 
-            if len(m) == 0:
-                m.append(nums[i])
-            elif nums[i] <= m[-1]:
-                m.append(nums[i])
-            else:
-                while len(m) != 0 and m[-1] < nums[i]:
-                    m.pop()
-                m.append(nums[i])
-
-            # Add current maximum.
-            res.append(m[0])
+            # Pop out from right the ones not large than current one.
+            while len(s) > 0 and nums[s[-1]] <= nums[i]:
+                s.pop()
+                
+            s.append(i)
+                
+            if i >= k - 1:
+                res.append(nums[s[0]])
 
         return res
